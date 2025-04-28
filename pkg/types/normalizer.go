b9be444fbf27018cb1faea7ed135e30fdc954c47
gabemontero <gmontero@redhat.com>
2025-04-28 14:16:34 -0400

    change default techdoc url for john

--- pkg/types/normalizer.go
+++ pkg/types/normalizer.go
@@ -66,5 +66,5 @@ const (
 // These const represent the curated techdocs repos we provide for certain models in the RHOAI model catalog
 const (
 	Granite318bLabName     = "granite-31-8b-lab"
-	Granite318bLabTechDocs = "https://github.com/redhat-ai-dev/granite-3.1-8b-lab-docs"
+	Granite318bLabTechDocs = "https://github.com/redhat-ai-dev/granite-3.1-8b-lab-docs/tree/main"
 )
