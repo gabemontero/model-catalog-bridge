5176ca77cc4bef496eaaf822f3230a59e108fba6
gabemontero <gmontero@redhat.com>
2025-08-06 15:13:13 -0400

    fetch modelcards from kubeflow, seed jsonarray with location url, push modelcard content to location service

--- pkg/cmd/cli/kubeflowmodelregistry/catalogsource.go
+++ pkg/cmd/cli/kubeflowmodelregistry/catalogsource.go
@@ -1,9 +1,22 @@
 package kubeflowmodelregistry
 
 import (
-	"github.com/kubeflow/model-registry/catalog/pkg/openapi"
+     "encoding/json"
+
+     "github.com/kubeflow/model-registry/catalog/pkg/openapi"
+     "github.com/redhat-ai-dev/model-catalog-bridge/pkg/rest"
 )
 
-type CatalogSourceWrapper struct {
-	catalogSource *openapi.CatalogSource
+func (k *KubeFlowRESTClientWrapper) ListCatalogSources() ([]openapi.CatalogSource, error) {
+     buf, err := k.getFromModelRegistry(k.RootCatalogURL + rest.LIST_CATALOG_SOURECES_URI)
+     if err != nil {
+          return nil, err
+     }
+
+     csl := openapi.CatalogSourceList{}
+     err = json.Unmarshal(buf, &csl)
+     if err != nil {
+          return nil, err
+     }
+     return csl.Items, nil
 }
